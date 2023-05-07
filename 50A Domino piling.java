7ed834166d889902c03d1bd66c4300bae2c557be
Mohamed <d2105944@hotmail.com>
2023-05-07 13:56:11 +0300

    math

---
+++ 50A Domino piling.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class answer {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		System.out.println((m * n) / 2);
+	}
+}
